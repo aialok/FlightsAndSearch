44b647e59af5d484f7c6b7897835b8d3f6f5389a
aialok <alokgupta1560@gmail.com>
2023-08-20 20:20:13 +0530

    added bodyParser

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const express=require('express');
-const {PORT}=require("./config/serverConfig") 
+const bodyParser=require('body-parser');
+
+const {PORT}=require("./config/serverConfig"); 
+const { blobFromSync } = require('node-fetch');
 
 const setupAndStartServer= async()=>{
     
     //create the express object
+     const app=express();
+     
+     app.use(bodyParser.json());
+     app.use(bodyParser.urlencoded({extended : true}));
+
     
-    const app=express();
     app.listen(PORT, ()=>{
         console.log(`Server started at ${PORT}`);
     });  
